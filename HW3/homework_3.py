af16388fdca08a413bc576f6c3003fb2e8be962f
glukhodednastya <glukhodednastya@gmail.com>
2022-10-12 12:44:51 +0300

    Edit hw3

--- HW3/homework_3.py
+++ HW3/homework_3.py
@@ -1,25 +1,40 @@
 from typing import List
+from collections import Counter
+
 
 class CountVectorizer():
     def __init__(self):
-        self.feature_names = list()
+        self.feature_names = []
+        self.frequency = []
+        self.corpus = []
 
-    def fit_transform(self, corpus: List[str]) -> List[List[int]]:
-        feature_names = [word.lower() for sent in corpus for word in sent.split()]
-        self.feature_names = list(feature_names)
-        return self.count_matrix(corpus)
+    def fit(self, text_corpus: List[str]) -> None:
+        features = []
+        for index, text in enumerate(text_corpus):
+            words_list = text.lower().split()
+            self.frequency.append(Counter(words_list))
+            self.corpus.append(words_list)
+            for word in words_list:
+                if word not in features:
+                    features.append(word)
+            self.feature_names = features
 
-    def count_matrix(self, corpus: List[str]) -> List[List[int]]:
-        matrix = list()
-        for sent in corpus:
-            matrix.append([])
-            for word in self.feature_names:
-                sent_words = [word.lower() for word in sent.split()]
-                matrix[-1].append(sent_words.count(word))
+    def count_matrix(self) -> List[List[int]]:
+        matrix = []
+        for index, _ in enumerate(self.corpus):
+            vector = []
+            for feature in self.feature_names:
+                vector.append(self.frequency[index][feature])
+            matrix.append(vector)
         return matrix
+
+    def fit_transform(self, text_corpus: List[str]) -> List[List[int]]:
+        self.fit(text_corpus)
+        return self.count_matrix()
 
     def get_feature_names(self) -> List[str]:
         return self.feature_names
+
 
 if __name__ == '__main__':
     corpus = [
@@ -30,4 +45,3 @@ if __name__ == '__main__':
     count_matrix = vectorizer.fit_transform(corpus)
     print(vectorizer.get_feature_names())
     print(count_matrix)
-
